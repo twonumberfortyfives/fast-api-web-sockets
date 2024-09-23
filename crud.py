2c59d4aad03c4bf81e6bf191a16ddfc2f144b9cf
twonumberfortyfives <sma4no.7@gmail.com>
2024-09-23 10:19:13 +0300

    implemented registration with hashed password

--- crud.py
+++ crud.py
@@ -1,6 +1,10 @@
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from db import models
 import schemas
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_all_users(db: Session):
@@ -19,11 +23,20 @@ def get_user_by_email(db: Session, email: str):
     )
 
 
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password, hashed_password)
+
+
 def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = hash_password(user.password)
     db_user = models.DBUser(
         email=user.email,
         username=user.username,
-        password=user.password
+        password=hashed_password,
     )
     db.add(db_user)
     db.commit()
