5abe168b54fb4cb865786c15da1e10b2ed055ff6
twonumberfortyfives <sma4no.7@gmail.com>
2024-09-23 12:43:55 +0300

    docker plus docker compose have been implemented

--- db/engine.py
+++ db/engine.py
@@ -1,8 +1,13 @@
+import os
+import dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./forum.db"
+dotenv.load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
