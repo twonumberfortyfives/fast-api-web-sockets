c38a671e8c4527368aa657ab2b7662beeb77cdeb
twonumberfortyfives <sma4no.7@gmail.com>
2024-09-24 10:23:41 +0300

    trying to fix cors error

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ app = FastAPI()
 
 
 origins = [
-    "http://127.0.0.1:5173",
-    "http://localhost:8000",
+    "http://localhost:5173",
+    "http://localhost",
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
