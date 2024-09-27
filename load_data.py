ae2cec136e6b4bbdb172eee84d0bf05347c47d53
twonumberfortyfives <sma4no.7@gmail.com>
2024-09-27 14:02:15 +0300

    test data for users and posts have been created.

--- load_data.py
+++ load_data.py
@@ -3,37 +3,39 @@ from db.engine import async_session
 from db.models import DBUser, DBPost
 
 
-async def load_extended_sample_data(json_file: str):
+async def load_extended_sample_data(test_users_data: str, test_posts_data: str):
     async with async_session() as session:
         async with session.begin():
             await session.execute(DBUser.__table__.delete())
             await session.execute(DBPost.__table__.delete())
 
-        with open(json_file, "r") as f:
+        with open(test_users_data, "r") as f:
             data = json.load(f)
 
-        async with session.begin():
-            for user_data in data["users"]:
-                user = DBUser(
-                    id=user_data["id"],
-                    email=user_data["email"],
-                    username=user_data["username"],
-                    password=user_data["password"],
-                    role=user_data["role"],
-                )
-                session.add(user)
-                for post_data in user_data["posts"]:
-                    post = DBPost(
-                        id=post_data["id"],
-                        topic=post_data["topic"],
-                        content=post_data["content"],
-                        user_id=user.id,
+            async with session.begin():
+                for user_data in data:
+                    user = DBUser(
+                        email=user_data["email"],
+                        username=user_data["username"],
+                        password=user_data["password"],
+                        profile_picture=user_data["profile_picture"],
+                        role=user_data["role"],
                     )
-                    session.add(post)
+                    session.add(user)
+        with open(test_posts_data, "r") as f:
+            data = json.load(f)
+            for post_data in data:
+                post = DBPost(
+                    topic=post_data["topic"],
+                    content=post_data["content"],
+                    user_id=post_data["user_id"],
+                    tags=post_data["tags"],
+                )
+                session.add(post)
         await session.commit()
 
 
 if __name__ == "__main__":
     import asyncio
 
-    asyncio.run(load_extended_sample_data("test_data.json"))
+    asyncio.run(load_extended_sample_data("test_users_data.json", "test_posts_data.json"))
