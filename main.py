c00080a8f03798b59e35b832f8acc54a9207ad2e
twonumberfortyfives <sma4no.7@gmail.com>
2024-10-09 12:38:56 +0300

    expose headers added

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["X-all-posts-count"],
 )
 
 app.include_router(users_router, prefix="/api", tags=["users"])
