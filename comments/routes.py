bd6f21c25c393fb833b47e1a97982cad4ea88b03
twonumberfortyfives <sma4no.7@gmail.com>
2024-10-21 19:55:52 +0300

    added remove feature for comments

--- comments/routes.py
+++ comments/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request, Response
 from fastapi_pagination import paginate, Page
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -17,3 +17,12 @@ async def get_comments(
     return paginate(await views.get_all_comments_view(db=db, post_id=post_id))
 
 
+@router.delete("/posts/{post_id}/all-comments/{comment_id}/")
+async def delete_comment(
+    post_id: int,
+    comment_id: int,
+    request: Request,
+    response: Response,
+    db: AsyncSession = Depends(get_db)
+):
+    return await views.delete_comment(request=request, response=response, post_id=post_id, comment_id=comment_id, db=db)
--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
+from fastapi import Response, Request
+from fastapi.exceptions import HTTPException
 
 from db import models
+from dependencies import get_current_user
 
 
 async def get_all_comments_view(db: AsyncSession, post_id: int):
@@ -16,3 +19,21 @@ async def get_all_comments_view(db: AsyncSession, post_id: int):
     )
     comments = comments_query.scalars().all()
     return comments
+
+
+async def delete_comment(request: Request, response: Response, post_id: int, comment_id: int, db: AsyncSession):
+    current_user_id = (await get_current_user(request=request, response=response, db=db)).id
+    current_comment_payload = await db.execute(
+        select(models.DBComment)
+        .filter(models.DBComment.post_id == post_id)
+        .filter(models.DBComment.id == comment_id)
+        .filter(models.DBComment.user_id == current_user_id)
+    )
+
+    current_comment = current_comment_payload.scalars().first()
+
+    if current_comment:
+        await db.delete(current_comment)
+        await db.commit()
+        return {"message": "The comment has been deleted."}
+    raise HTTPException(status_code=400, detail="An error has been occurred.")
